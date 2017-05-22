31d9dcf83df25ad866d181031ebbe17d4d574230
Truong Duc Hanh <htruongduc@pentalog.fr>
2017-05-22 16:29:56 +0700

    Seperate into a new component for detail page from master page

---
+++ src/app/hero.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by htruongduc on 5/22/2017.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by htruongduc on 5/22/2017.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
